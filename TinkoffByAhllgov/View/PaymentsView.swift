cf03f143b445712c1ad73a1aa22bcfa8adf192d0
Ahllgov <magaahilgov1@gmail.com>
2021-10-03 19:12:16 +0300

    Services Tab Page

--- TinkoffByAhllgov/View/PaymentsView.swift
+++ TinkoffByAhllgov/View/PaymentsView.swift
@@ -35,7 +35,7 @@ struct PaymentsView: View {
                         Image(systemName: "magnifyingglass")
                             .font(.system(size: 20))
                             .foregroundColor(.gray)
-                            .padding(.leading)
+                            .padding(.leading,5)
                         TextField("Операции", text: $searchOperations)
                             .foregroundColor(Color.black)
                     }
@@ -46,9 +46,10 @@ struct PaymentsView: View {
                 .padding(.horizontal)
                 //Избранное
                 VStack (spacing: 10){
-                    HStack{Text("Избранное")
-                        .font(.title2)
-                        .fontWeight(.bold)
+                    HStack{
+                        Text("Избранное")
+                            .font(.title2)
+                            .fontWeight(.bold)
                         Spacer()
                         Text("Еще")
                             .foregroundColor(.blue)
@@ -66,13 +67,161 @@ struct PaymentsView: View {
                                         .font(.system(size: 10))
                                         .lineLimit(3)
                                         .padding(.vertical, 6)
-                                }.frame(width: 70, height: 110)
-                                .padding(.horizontal,15)
+                                }.frame(width: 50, height: 110)
+                                .padding(.horizontal,25)
                             }
                         }
                     }
                 }
                 .frame(width: UIScreen.main.bounds.width, height: 150)
+                //Переводы по телефону
+                VStack {
+                    VStack(alignment: .leading){
+                        Text("Переводы по телефону")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        HStack {
+                            Image(systemName: "phone.fill")
+                                .font(.system(size: 20))
+                                .foregroundColor(.blue)
+                                .padding(.leading,20)
+                            TextField("Введите номер или имя", text: $searchOperations)
+                                .foregroundColor(Color.black)
+                        }
+                        .background(RoundedRectangle(cornerRadius: 15).frame(width: UIScreen.main.bounds.width - 35 , height: 40))
+                        .foregroundColor(.gray.opacity(0.15))
+                        .padding(.top)
+                    }
+                    .padding()
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack(spacing: -35) {
+                            ForEach(0 ..< 15) { item in
+                                VStack {
+                                    Image(systemName: "person.crop.circle.fill")
+                                        .font(.system(size: 25, weight: .heavy, design: .rounded))
+                                        .foregroundColor(.white)
+                                        .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2639923096, green: 0.5612514615, blue: 1, alpha: 1)),Color(#colorLiteral(red: 0.2031532824, green: 0.4292642772, blue: 0.7896229625, alpha: 1))]), startPoint: .top, endPoint: .bottom).frame(width: 50, height: 50).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/))
+                                    Text("Имя Фамилия")
+                                        .multilineTextAlignment(.center)
+                                        .font(.system(size: 10))
+                                        .lineLimit(3)
+                                        .padding(.vertical, 10)
+                                }.frame(width: 50, height: 110)
+                                .padding(.horizontal,25)
+                            }
+                        }
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width, height: 150)
+                .padding(.top,5)
+                //Переводы
+                VStack (spacing: 10){
+                    HStack{
+                        Text("Переводы")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Spacer()
+                        Text("Еще")
+                            .foregroundColor(.blue)
+                    }
+                    .padding(.horizontal)
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack(spacing: -35) {
+                            ForEach(0 ..< 9) { item in
+                                ZStack(alignment: .topLeading) {
+                                    Image(systemName: "rublesign.circle.fill")
+                                        .font(.system(size: 25))
+                                        .foregroundColor(.white)
+                                        .background(Color.blue.frame(width: 40, height: 40).clipShape(Circle()))
+                                        .padding(10)
+                                    Text("Как переводим: на карту, меж счетами, по реквизитам")
+                                        .foregroundColor(Color.black)
+                                        .lineLimit(2)
+                                        .font(.system(size: 10))
+                                        .padding(5)
+                                        .frame(width: 90, height: 90, alignment: .bottomLeading)
+                                }
+                                .background(Color.white.frame(width: 100, height: 100).cornerRadius(15).shadow(color: .gray.opacity(0.5), radius: 5))
+                                .frame(width: 120, height: 120)
+                                .padding(.horizontal,15)
+                            }
+                        }
+                    }.padding(.horizontal,-5)
+                }
+                .frame(width: UIScreen.main.bounds.width, height: 150)
+                .padding(.top, 15)
+                //Платежи
+                VStack (spacing: 10){
+                    HStack{
+                        Text("Платежи")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Spacer()
+                        Text("Еще")
+                            .foregroundColor(.blue)
+                    }
+                    .padding(.horizontal)
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack(spacing: -35) {
+                            ForEach(0 ..< 9) { item in
+                                ZStack(alignment: .topLeading) {
+                                    Image(systemName: "house.fill")
+                                        .font(.system(size: 25))
+                                        .foregroundColor(.white)
+                                        .background(Color.blue.frame(width: 40, height: 40).clipShape(Circle()))
+                                        .padding(10)
+                                    Text("Коммуналка, Госуслуги, Штрафы")
+                                        .foregroundColor(Color.black)
+                                        .lineLimit(2)
+                                        .font(.system(size: 10))
+                                        .padding(5)
+                                        .frame(width: 90, height: 90, alignment: .bottomLeading)
+                                }
+                                .background(Color.white.frame(width: 100, height: 100).cornerRadius(15).shadow(color: .gray.opacity(0.5), radius: 5))
+                                .frame(width: 120, height: 120)
+                                .padding(.horizontal,15)
+                            }
+                        }
+                    }.padding(.horizontal,-5)
+                }
+                .frame(width: UIScreen.main.bounds.width, height: 150)
+                .padding(.top, 15)
+                //Действия
+                VStack (spacing: 10){
+                    HStack{
+                        Text("Действия")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Spacer()
+                        Text("Еще")
+                            .foregroundColor(.blue)
+                    }
+                    .padding(.horizontal)
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack(spacing: -35) {
+                            ForEach(0 ..< 9) { item in
+                                ZStack(alignment: .topLeading) {
+                                    Image(systemName: "banknote.fill")
+                                        .font(.system(size: 25))
+                                        .foregroundColor(.white)
+                                        .background(Color.blue.frame(width: 40, height: 40).clipShape(Circle()))
+                                        .padding(15)
+                                    Text("Снять наличные, запросить деньги")
+                                        .foregroundColor(Color.black)
+                                        .lineLimit(2)
+                                        .font(.system(size: 10))
+                                        .padding(5)
+                                        .frame(width: 90, height: 90, alignment: .bottomLeading)
+                                }
+                                .background(Color.white.frame(width: 100, height: 100).cornerRadius(15).shadow(color: .gray.opacity(0.5), radius: 5))
+                                .frame(width: 120, height: 120)
+                                .padding(.horizontal,15)
+                            }
+                        }
+                    }.padding(.horizontal,-5)
+                }
+                .frame(width: UIScreen.main.bounds.width, height: 150)
+                .padding(.top, 15)
             }
         }
     }
